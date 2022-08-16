f7b3115146ef8e438670078feaf2907b8e57d4b2
dazy1030 <dev.nodajima@gmail.com>
2022-08-16 21:56:49 +0900

    Initial Commit

---
+++ SwiftUIQuestionSample/SwiftUIQuestionSampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIQuestionSampleApp.swift
+//  SwiftUIQuestionSample
+//
+//  Created by Naoki Odajima on 2022/08/16.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIQuestionSampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
